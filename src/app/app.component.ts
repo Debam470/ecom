83e78534aa309a2521eeb583fa7bde5170f114d0
debam470 <debam470@gmail.com>
2020-12-17 22:59:29 +0530

    My Second Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  
+  constructor() {
+  }
+  ngOnInit() {
+  }
+  ngOnDestroy() {
+  }
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  formData = {title0: '', title1: '', title2: '', title3: ''};
+  constructor() { }
+
+  ngOnInit() {
+    this.formData.title0 = 'PURCHASE ANY THREE ITEMS GET 20% OFF | NO CODE NEEDED';
+    this.formData.title1 = 'AVOID SHIPPING DELAYS & ORDER EARLY';
+    this.formData.title2 = 'BUY ONLINE & PICK UP IN STORES NOW';
+    this.formData.title3 = 'CALL US AT 678.989.2006 TO TALK TO A STYLIST  ';
+  }
+
+}
